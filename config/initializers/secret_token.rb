6aac4d9df83a9fa432dd65c1685ca5de24124e18
Gumaro Melendez <cloudernew@gmail.com>
2013-02-14 22:14:06 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkoutTracker::Application.config.secret_token = '85c128babb7aec6221740e75d831ae47b7114126712347d0139c3e652c22880283e675741b7362a38277f7f34184f3b4d22ce9097473a7f7cbab2b12d043af85'
